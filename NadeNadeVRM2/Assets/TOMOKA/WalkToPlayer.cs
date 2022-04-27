f8bb2b2d4a957bc018b81e4c108fe64bc7b26f79
argiltom <tomo0118@live.jp>
2022-04-28 08:13:48 +0900

    push

---
+++ NadeNadeVRM2/Assets/TOMOKA/WalkToPlayer.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkToPlayer : MonoBehaviour
+{
+    protected enum Situation {Stop,Walk,Run};
+    [SerializeField] VRMAnimControler vrmAnimControler;
+    [SerializeField] BaseVRMAnimEnum idleAnim;
+    [SerializeField] NavMeshAgent navMeshAgent;
+    [SerializeField] Transform target;
+    /// <summary>
+    /// x:í‚é~ãóó£ y:ï‡çsãóó£ z:ëñçsãóó£
+    /// </summary>
+    [SerializeField] Vector3 controllDistance;
+    [SerializeField] float walkSpeed;
+    [SerializeField] float runSpeed;
+    Situation situation;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMeshAgent.destination = target.position;
+        UpdateSituation();
+        UpdateBehavior();
+        
+    }
+    void UpdateSituation()
+    {
+        float distance = (navMeshAgent.destination - transform.position).magnitude;
+        if(distance <= controllDistance.x)
+        {
+            situation = Situation.Stop;
+        }else if(distance <= controllDistance.y)
+        {
+            situation= Situation.Walk;
+        }
+        else if(distance <= controllDistance.z)
+        {
+            situation = Situation.Run;
+        }
+    }
+    void UpdateBehavior()
+    {
+        switch (situation)
+        {
+            case Situation.Stop:
+                navMeshAgent.speed = 0;
+                navMeshAgent.acceleration = 0;
+                vrmAnimControler.BaseAnimEnum = idleAnim;
+                break;
+            case Situation.Walk:
+                navMeshAgent.speed = walkSpeed;
+                navMeshAgent.acceleration = 10;
+                vrmAnimControler.BaseAnimEnum = BaseVRMAnimEnum.WALK00_F;
+                break;
+            case Situation.Run:
+                navMeshAgent.speed = runSpeed;
+                navMeshAgent.acceleration = 10;
+                vrmAnimControler.BaseAnimEnum = BaseVRMAnimEnum.RUN00_F;
+                break;
+        }
+    }
+}
